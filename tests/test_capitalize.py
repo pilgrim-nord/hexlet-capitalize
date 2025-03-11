d935484695e8857ef6d145f1d7c918aa36d60804
pilgrim-nord <dmelekhov@gmail.com>
2025-03-11 14:57:45 +0300

    start

---
+++ tests/test_capitalize.py
@@ -0,0 +1,14 @@
+# Нажмите кнопку Run чтобы запустить тесты.
+
+# Файлы с исходным кодом можно посмотреть на вкладке "Files":
+# src/capitalize.py        - тестируемая функция
+# tests/test_capitalize.py - тесты функции
+
+# Попробуйте изменять код функции/тестов, запуская проверки заново.
+
+from capitalize import capitalize
+
+assert capitalize('hello') == 'Hello'
+assert capitalize('') == ''
+
+print('Все тесты пройдены!')
